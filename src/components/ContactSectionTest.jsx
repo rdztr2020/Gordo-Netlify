92682fa5e89f9467199934ddcecf7c24a4a61a31
windowslee <slee12463@gmail.com>
2024-10-18 21:51:13 -0400

    link hover

--- src/components/ContactSectionTest.jsx
+++ src/components/ContactSectionTest.jsx
@@ -6,7 +6,7 @@ const ContactSection2 = () => {
         <p className="text-center text-2xl tracking-tighter lg:text-4xl pb-6 text-white">
           Address
         </p>
-        <a href="https://www.google.com/maps/place/29735+NY-10,+Walton,+NY+13856/@42.1544303,-75.0907944,16z/data=!4m6!3m5!1s0x89db791c1918ca8f:0x773c8ad49ac947d1!8m2!3d42.1544303!4d-75.0907944!16s%2Fg%2F11rkcd0mzy"><p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2">
+        <a className="hover:text-sky-700" href="https://www.google.com/maps/place/29735+NY-10,+Walton,+NY+13856/@42.1544303,-75.0907944,16z/data=!4m6!3m5!1s0x89db791c1918ca8f:0x773c8ad49ac947d1!8m2!3d42.1544303!4d-75.0907944!16s%2Fg%2F11rkcd0mzy"><p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2">
           29735 NY-10
         </p>
         <p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2">
@@ -18,11 +18,11 @@ const ContactSection2 = () => {
         <p className="text-center text-2xl tracking-tighter lg:text-4xl pt-4 pb-6 text-white">
           Phone
         </p>
-        <a href="tel:+6075100100"><p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2">(607) 510-0100</p></a>
+        <p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2"><a href="tel:+6075100100" className="hover:text-sky-700">(607) 510-0100</a></p>
         <p className="text-center text-2xl tracking-tighter lg:text-4xl pt-4 pb-6 text-white">
           Email
         </p>
-        <a href="mailto: slee12463@gmail.com"><p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2">gordomike@gordomikes</p></a>
+        <p className="text-center text-2xl tracking-tighter lg:text-4xl pb-2"><a href="mailto: slee12463@gmail.com" className="hover:text-sky-700">gordomike@gordomikes</a></p>
       </div>
     </section>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
     <div className="mb-8 mt-20">
       <div className="flex items-center justify-center gap-8">
       {SOCIAL_MEDIA_LINKS.map((link,index)=>(
-        <a key={index} href={link.href} target="_blank" rel="noopener noreferrer">
+        <a key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:text-sky-700 text-sky-300">
           {link.icon}
         </a>
       ))}
       </div>
-      <p className="mt-8 text-center tracking-tighter text-neutral-500">
+      <p className="mt-8 text-center tracking-tighter text-orange-300">
         &copy; THE🤓NERD 2024<br/>All rights reserved.
       </p>
     </div>
