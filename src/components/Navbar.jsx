ddbe3bf7577ec2a322a9a24f22a67d437deb70c1
windowslee <slee12463@gmail.com>
2024-10-23 22:06:05 -0400

    fix logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,10 +25,12 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
+    <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center" >
       <div className="flex w-full items-center justify-between overflow-y-hidden p-2 backdrop-blur-sm lg:m-2 lg:w-[50rem]
-      lg:rounded-full lg:shadow-lg">
-        <img src={logo} alt="logo" width={150} height={150} />
+      lg:rounded-full lg:shadow-lg" ><a href="#">
+        <img src={logo} alt="logo" width={150} height={150}/>
+        </a>
+        
         <div className="hidden space-x-6 lg:flex">
           {LINKS.map((link, index) => (
             <a
